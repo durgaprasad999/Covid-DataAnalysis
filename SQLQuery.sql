377c986a3e88207070c82eeeb4df1573a14198a3
Venkat <venkatadurgaprasadpyla@outlook.com>
2023-01-18 15:12:49 +0530

    Update and rename SQLQuery1.sql to SQLQuery.sql
    I have given only some basic queries I have used to get relevant information for my project but remember the applications can be many and we can do much more from the data.

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,8 +1,8 @@
 select * from AnalystProject.dbo.CovidDeaths
 order by 3, 4
 
---select * from AnalystProject.dbo.CovidVaccinations
---order by 3, 4
+select * from AnalystProject.dbo.CovidVaccinations
+order by 3, 4
 
 select location, date, total_cases, new_cases, total_deaths, population
 from AnalystProject.dbo.CovidDeaths
@@ -12,7 +12,7 @@ order by 1,2
 --shows the likelyhood of dying
 select location, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 from AnalystProject.dbo.CovidDeaths
---where location like 'india'
+where location like 'india'
 group by location, total_cases, total_deaths
 order by 1,2
 
@@ -32,14 +32,12 @@ order by 4 desc
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From AnalystProject.dbo.CovidDeaths
---Where location like '%states%'
 Group by Location, Population, date
 order by PercentPopulationInfected desc
 
 --countries with highest death rate w.r.t population
 select location, max(cast(total_deaths as int)) as Highdeathcount, max((total_deaths/population))*100 as Percentpeopledied
 from AnalystProject.dbo.CovidDeaths
---where location like 'india'
 --where continent is not null
 group by location
 order by 2 desc
@@ -51,11 +49,10 @@ from AnalystProject.dbo.CovidDeaths
 where continent is not null
 group by continent
 order by 2 desc
---
---Removing noise in  data
+
+--Removing noise in  data, I have some noise in my database, which got added to my covid data from another table in my database, I have removed simply using "not in"
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From AnalystProject.dbo.CovidDeaths
---Where location like '%states%'
 Where continent is null 
 and location not in ('World', 'European Union', 'International', 'high income', 'upper middle income', 'Lower middle income', 'Low income')
 Group by location
@@ -78,54 +75,3 @@ join AnalystProject.dbo.CovidVaccinations cov
 	and cod.date=cov.date
 where cod.continent is not null
 order by 2,3
-
---example to use CTE
-with pvsv (continent, location, date, population, RollingPeopleVaccinated, new_vaccinations) 
-as(
-select cod.continent, cod.location, cod.date, cod.population, cov.new_vaccinations
-, sum(convert(int,cov.new_vaccinations)) over (partition by cod.location order by cod.location, cod.date) AS RollingPeopleVaccinated
-from AnalystProject.dbo.CovidDeaths cod 
-join AnalystProject.dbo.CovidVaccinations cov
-	on cod.location=cov.location 
-	and cod.date=cov.date
-where cod.continent is not null
---order by 2,3
-)
-select*, (RollingPeopleVaccinated/population)*100
-from pvsv
-
---temp table
-drop table if exists newpeoplevaccinated 
-create table newpeoplevaccinated 
-(
-continent nvarchar(255),
-location nvarchar(255),
-date datetime,
-population numeric,
-new_vaccinations numeric,
-RollingPeopleVaccinated numeric) 
-
-insert into newpeoplevaccinated  
-select cod.continent, cod.location, cod.date, cod.population, cov.new_vaccinations
-, sum(convert(numeric,cov.new_vaccinations)) over (partition by cod.location order by cod.location, cod.date) AS RollingPeopleVaccinated
-from AnalystProject.dbo.CovidDeaths cod 
-join AnalystProject.dbo.CovidVaccinations cov
-	on cod.location=cov.location 
-	and cod.date=cov.date
---where cod.continent is not null
---order by 2,3
-
-select*, (RollingPeopleVaccinated/population)*100
-from newpeoplevaccinated 
-
-
---create view
-create view npvperday as
-select cod.continent, cod.location, cod.date, cod.population, cov.new_vaccinations
-, sum(convert(numeric,cov.new_vaccinations)) over (partition by cod.location order by cod.location, cod.date) AS RollingPeopleVaccinated
-from AnalystProject.dbo.CovidDeaths cod 
-join AnalystProject.dbo.CovidVaccinations cov
-	on cod.location=cov.location 
-	and cod.date=cov.date
-where cod.continent is not null
---order by 2,3
